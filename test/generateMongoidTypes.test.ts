5c34459d01e5deab27d1e1691e2d21554b940342
Ashok Dongare <ashok.dongare@semanticbits.com>
2020-05-31 18:25:29 -0400

    MAT-1049 trigger build

--- test/generateMongoidTypes.test.ts
+++ test/generateMongoidTypes.test.ts
@@ -1,4 +1,4 @@
-import { exec } from 'child_process';
+import {exec} from 'child_process';
 import fs from 'fs';
 import path from 'path';
 import { mongoidPrimitiveTypes } from "../src/model/dataTypes/primitiveDataTypes";
