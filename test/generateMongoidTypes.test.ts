57f91377be75064a5e1fc3b6d338b5e633d8bdd4
Ashok Dongare <ashok.dongare@semanticbits.com>
2020-06-22 19:10:16 -0400

    MAT-1168 revert the change

--- test/generateMongoidTypes.test.ts
+++ test/generateMongoidTypes.test.ts
@@ -1,19 +1,29 @@
-import { exec } from 'child_process';
-import execPromise from './execPromise';
+import MongoidTypeGenerator from "../src/generators/MongoidTypeGenerator";
+import { entityCollection } from './fixture/entityCollection.json'
+import EntityCollection from "../src/model/dataTypes/EntityCollection";
 
 describe('generateMongoidTypes', () => {
 
-  const modelDir = './test/mongoid';
-  const generatorScript = './src/generateMongoidTypes.ts';
-
-  // remove models after all tests have been run
-  afterAll(() => {
-    exec(`rm -rf ${modelDir}`);
-  });
-
   test('Should generate mongoid models successfully', async() => {
-    const cmd = `ts-node ${generatorScript} --output-directory=${modelDir}`;
-    const result = await  execPromise(cmd);
-    expect(result).toContain('Successfully generated 932 types');
+    const collection = <EntityCollection>JSON.parse(JSON.stringify(entityCollection));
+    const result = await  MongoidTypeGenerator(collection);
+    expect(result.length).toEqual(collection.entities.length);
+    expect(result[0]).toContain('module FHIR\n' +
+      '  # FHIR/Account.rb\n' +
+      '  class Account < DomainResource\n' +
+      '    include Mongoid::Document\n' +
+      '    field :typeName, type: String, default: \'Account\'\n' +
+      '    embeds_many :identifier, class_name: \'Identifier\'\n' +
+      '    embeds_one :status, class_name: \'AccountStatus\'\n' +
+      '    embeds_one :type, class_name: \'CodeableConcept\'\n' +
+      '    field :name, type: String\n' +
+      '    embeds_many :subject, class_name: \'Reference\'\n' +
+      '    embeds_one :servicePeriod, class_name: \'Period\'\n' +
+      '    embeds_many :coverage, class_name: \'AccountCoverage\'\n' +
+      '    embeds_one :owner, class_name: \'Reference\'\n' +
+      '    field :description, type: String\n' +
+      '    embeds_many :guarantor, class_name: \'AccountGuarantor\'\n' +
+      '    embeds_one :partOf, class_name: \'Reference\'\n' +
+      '  end\n');
   });
 });
