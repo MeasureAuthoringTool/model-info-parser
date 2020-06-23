9c809bcb067903f9e509016a5178c15a8abaf7de
Andrew Bird <andrew.bird@semanticbits.com>
2020-06-23 16:42:47 -0500

    MAT-1081: fixing broken test

--- test/generateMongoidTypes.test.ts
+++ test/generateMongoidTypes.test.ts
@@ -16,23 +16,23 @@ describe("generateMongoidTypes", () => {
     const result = await MongoidTypeGenerator(collection);
     expect(result.length).toEqual(collection.entities.length);
     expect(result[0]).toContain(
-      "module FHIR\n" +
-        "  # FHIR/Account.rb\n" +
-        "  class Account < DomainResource\n" +
-        "    include Mongoid::Document\n" +
-        "    field :typeName, type: String, default: 'Account'\n" +
-        "    embeds_many :identifier, class_name: 'Identifier'\n" +
-        "    embeds_one :status, class_name: 'AccountStatus'\n" +
-        "    embeds_one :type, class_name: 'CodeableConcept'\n" +
-        "    field :name, type: String\n" +
-        "    embeds_many :subject, class_name: 'Reference'\n" +
-        "    embeds_one :servicePeriod, class_name: 'Period'\n" +
-        "    embeds_many :coverage, class_name: 'AccountCoverage'\n" +
-        "    embeds_one :owner, class_name: 'Reference'\n" +
-        "    field :description, type: String\n" +
-        "    embeds_many :guarantor, class_name: 'AccountGuarantor'\n" +
-        "    embeds_one :partOf, class_name: 'Reference'\n" +
-        "  end\n"
+      `module FHIR
+  # FHIR/Account.rb
+  class Account < DomainResource
+    include Mongoid::Document
+    field :typeName, type: String, default: 'Account'
+    embeds_many :identifier, class_name: 'Identifier'
+    embeds_one :status, class_name: 'AccountStatus'
+    embeds_one :type, class_name: 'CodeableConcept'
+    embeds_one :name, class_name: 'PrimitiveString'
+    embeds_many :subject, class_name: 'Reference'
+    embeds_one :servicePeriod, class_name: 'Period'
+    embeds_many :coverage, class_name: 'AccountCoverage'
+    embeds_one :owner, class_name: 'Reference'
+    embeds_one :description, class_name: 'PrimitiveString'
+    embeds_many :guarantor, class_name: 'AccountGuarantor'
+    embeds_one :partOf, class_name: 'Reference'
+`
     );
   });
 });
