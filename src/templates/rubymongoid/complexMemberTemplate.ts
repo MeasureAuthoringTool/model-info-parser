073a61bf0121fc08bd8f98de36c6e0000e1be8fd
Ashok Dongare <ashok.dongare@semanticbits.com>
2020-06-25 09:19:56 -0400

    MAT-1179 System type should not be embedded

--- src/templates/rubymongoid/complexMemberTemplate.ts
+++ src/templates/rubymongoid/complexMemberTemplate.ts
@@ -1,3 +1,5 @@
-export default "{{ member.relationshipType }} :{{ prefixVariableName member.variableName }}, " +
+export default "{{#if member.dataType.systemType}}field :{{ prefixVariableName member.variableName }}, " +
+  "type: {{#getMongoidPrimitive member.dataType.typeName}}{{/getMongoidPrimitive}}" +
+  "{{else}}{{ member.relationshipType }} :{{ prefixVariableName member.variableName }}, " +
   "class_name: '{{ member.dataType.normalizedName }}'" +
-  "{{# unless member.bidirectional }}, inverse_of: nil{{/ unless }}";
+  "{{#unless member.bidirectional }}, inverse_of: nil{{/unless }}{{/if}}";
