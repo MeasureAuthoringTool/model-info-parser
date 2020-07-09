4f7352c7a02349f52357fadd9d5d257efb87e457
Ashok Dongare <ashok.dongare@semanticbits.com>
2020-07-09 10:04:09 -0400

    MAT-1240 uninitialized constants issue while persisting models

--- src/templates/rubymongoid/complexMemberTemplate.ts
+++ src/templates/rubymongoid/complexMemberTemplate.ts
@@ -1,5 +1,5 @@
 export default "{{#if member.dataType.systemType}}field :{{ prefixVariableName member.variableName }}, " +
   "type: {{#getMongoidPrimitive member.dataType.typeName}}{{/getMongoidPrimitive}}" +
   "{{else}}{{ member.relationshipType }} :{{ prefixVariableName member.variableName }}, " +
-  "class_name: '{{ member.dataType.normalizedName }}'" +
+  "class_name: '{{member.dataType.namespace}}::{{ member.dataType.normalizedName }}'" +
   "{{#unless member.bidirectional }}, inverse_of: nil{{/unless }}{{/if}}";
