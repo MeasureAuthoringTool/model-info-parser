c21d0ed28ad4d86fadcf04dd83d4faa6b5cd3ef9
Serhii Ilin <serhii.ilin@semanticbits.com>
2020-07-28 09:32:08 -0400

    MAT-1467 Mongoid choice type as separate fields

--- src/templates/rubymongoid/complexMemberTemplate.ts
+++ src/templates/rubymongoid/complexMemberTemplate.ts
@@ -1,5 +1,5 @@
-export default "{{#if member.dataType.systemType}}field :{{ prefixVariableName member.variableName }}, " +
-  "type: {{#getMongoidPrimitive member.dataType.typeName}}{{/getMongoidPrimitive}}" +
-  "{{else}}{{ member.relationshipType }} :{{ prefixVariableName member.variableName }}, " +
-  "class_name: '{{member.dataType.namespace}}::{{ member.dataType.normalizedName }}'" +
-  "{{#unless member.bidirectional }}, inverse_of: nil{{/unless }}{{/if}}";
+export default "{{#if dataType.systemType}}field :{{ variableName }}, " +
+  "type: {{#getMongoidPrimitive dataType.typeName}}{{/getMongoidPrimitive}}" +
+  "{{else}}{{ relationshipType }} :{{ variableName }}, " +
+  "class_name: '{{ dataType.namespace }}::{{ dataType.normalizedName }}'" +
+  "{{#unless bidirectional }}, inverse_of: nil{{/unless }}{{/if}}";
